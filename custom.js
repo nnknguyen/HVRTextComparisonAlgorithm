ede302fde61afaf5b65f1293cf35c34efe281e46
khang_ngocnam_nguyen <ngoc-nam-khang.nguyen@senecacollege.ca>
2019-05-02 15:40:53 -0400

    adding contracted form identifying

--- custom.js
+++ custom.js
@@ -3,8 +3,8 @@ const cfh = require("./contractedFormHelpers");
 
 function tokenize(text) {
 	if (!text || text.trim() == "") return [];
-	let tokens = [];	// result
-	let register = []; // Conveying belt
+	let tokens = [];		// result
+	let register = []; 		// conveying belt
 	let detectApostrophe = false;
 	for (let i = 0; i <= text.length; i++) {
 		let newChar = text.charAt(i);
@@ -31,24 +31,51 @@ function tokenize(text) {
 	return tokens;
 }
 
-console.log(tokenize("He didn't say 'goodbye!' to me!").filter(c => c != ' '));
+//console.log(tokenize("He didn't say 'goodbye!' to me!");
 
-/*module.exports = function customCompare(text1, text2) {
+Array.prototype.containsSubArray = function(sub) {
+	if (!Array.isArray(sub)) throw new Error("containsSubArray() receives an Array");
+	let thisSize = this.length;
+	let subSize = sub.length;
+	for (let i = 0; i < thisSize - subSize; i++) {
+		if (this[i] === sub[0]) {
+			let matchedTokens = 0;
+			for (let j = 1; j < subSize; j++)
+				if (this[i + j] === sub[j]) matchedTokens++;
+			if (matchedTokens === subSize - 1) return true;
+		}
+	}
+	return false;
+}
+
+function intersect(arr1, arr2) {
+	return arr1.filter(token => {
+		if (arr2.includes(token)) return true;
+		let expanded = cfh.getExpandedForm(token);
+		if (expanded) {
+			for (let i = 0; i < expanded.length; i++) {
+				if (arr2.containsSubArray(expanded[i])) return true;
+			}
+		}
+		return false;
+	});
+}
+
+module.exports = function customCompare(text1, text2) {
     let filtered1 = tokenize(text1);
     let filtered2 = tokenize(text2);
     let arrayRatio = 0;
     let setRatio = 0;
-    let displayRatio = 0;
+	let displayRatio = 0;
+	let inter = intersect(filtered1, filtered2);
     if (filtered2.length >= filtered1.length) {
-        let inter = filtered1.filter(v => filtered2.includes(v));
         arrayRatio = inter.length / filtered1.length;
         setRatio = new Set(inter).size / new Set(filtered1).size;
         displayRatio = Math.max(arrayRatio, setRatio);
     } else {
-        let inter = filtered2.filter(v => filtered1.includes(v));
         arrayRatio = inter.length / filtered2.length;
         setRatio = new Set(inter).size / new Set(filtered2).size;
         displayRatio = Math.max(arrayRatio, setRatio);
     }
     return displayRatio;
-}*/
+}
