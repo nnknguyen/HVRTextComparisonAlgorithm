04112aa6903449ed5d3fcf01c3acb3393264ced8
khang_ngocnam_nguyen <ngoc-nam-khang.nguyen@senecacollege.ca>
2019-05-21 10:00:40 -0400

    add test cases

--- TestCases/spelltest.js
+++ TestCases/spelltest.js
@@ -1,10 +1,14 @@
 const spell = require('../spellcheck');
 
-let text = "I am no0t hangry because I already buyed sandwich from Subway.";
+let texts = ["I am no0t hangry because I already buyed sandwich from Subway."];
 
-spell(text).then(res => {
-    console.log('ORIGINAL: ' + text);
-    console.log('CORRECTED: ' + res.correct);
-    //console.log('DATA:');
-    //console.log(res.json);
-}).catch(err => console.log(err));
+texts.forEach(text => {
+    spell(text).then(res => {
+        console.log('ORIGINAL: ' + text);
+        console.log('CORRECTED: ' + res.correct);
+        console.log('DATA:');
+        console.log("__________");
+        console.log(res.json);
+        console.log("==========");
+    }).catch(err => console.log(err));
+});
