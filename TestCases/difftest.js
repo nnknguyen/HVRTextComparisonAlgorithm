fe13637d050b4ad5790fa684d2ccf6e2f38705bb
rayshroom <goodcolorpencils@gmail.com>
2019-05-26 23:02:21 -0400

    minor change to difftest.js

--- TestCases/difftest.js
+++ TestCases/difftest.js
@@ -1,16 +1,5 @@
 const Diff = require('../diff');
 
-var cases = [
-    {
-        text1: "I would not say, I couldn't have done the repair without you.",
-        text2: "I wouldn't say I could not have done the repair without you.",
-    },
-    {
-        text2: "I am hungry and I am going to buy some sandwich from Subway.",
-        text1: "I am not hungry because already bought sandwich from Subway."
-    },
-];
-
 text1 = "I would not say, I couldn't have done the repair without you.";
 text2 = "I wouldn't say I could not have done the repair without you.";
 
