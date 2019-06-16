3420e3882d84094840db0608078962c1d05b77fd
rayshroom <goodcolorpencils@gmail.com>
2019-06-16 00:22:01 -0400

    updated difftest with more test cases

--- TestCases/difftest.js
+++ TestCases/difftest.js
@@ -1,6 +1,24 @@
 const Diff = require('../diff');
+let texts = [
+    {   
+        text1 : "I would not say, I couldn't have done the repair without you.",
+        text2 : "I wouldn't say I could not have done the repair without you."
+    },
+    {   
+        text1 : "Where were you when we became World Champions? We want to see how you celebrated becoming @NBA Champs.",
+        text2 : "Where are you when we become the World Champion? We need to know how you celebrated winning #NBA finals."
+    },
+    {   
+        text1 : "I would not say, I couldn't have done the repair without you.",
+        text2 : "I wouldn't say I could not have done the repair without you."
+    },
+    {
+        text1 : "At all locations the stations were full of bikes — during one of the busiest three months for usage, according to a Nov. 2018 report on the program.",
+        text2 : "At all places were full of bikes — during one of the peak three months for usage, according to a Nov. 2018 report on the program."
+    }
+];
 
-text1 = "I would not say, I couldn't have done the repair without you.";
-text2 = "I wouldn't say I could not have done the repair without you.";
-
-console.log(Diff.diffWords(text1, text2));
+texts.forEach(t => {
+    console.log(Diff.diffWords(t.text1, t.text2));
+    console.log('-'.repeat(50));
+});
